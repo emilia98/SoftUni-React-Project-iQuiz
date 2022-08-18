18b25f74b69699115d67cc2df8f8ef73b1cba5c1
Emilia Nedyalkova <emilia_nedialkova@abv.bg>
2022-08-18 22:50:26 +0300

    add validators

--- iquiz/src/components/Authentication/Register/Register.js
+++ iquiz/src/components/Authentication/Register/Register.js
@@ -4,10 +4,10 @@ import AuthForm from "../../Common/AuthForm";
 const Register = () => {
     let fields = [
         { type: "text",  name: "fullName", placeholder: "Full Name", labelText: "Full Name", hasPreview: false, validator: AuthValidator.fullName },
-        { type: "email",  name: "email", placeholder: "Email", labelText: "Email", hasPreview: false},
-        { type: "password",  name: "password", placeholder: "Password", labelText: "Password", hasPreview: false }, 
-        { type: "password",  name: "confirmPassword", placeholder: "Confirm Password", labelText: "Confirm Password", hasPreview: false }, 
-      ]
+        { type: "email",  name: "email", placeholder: "Email", labelText: "Email", hasPreview: false,},
+        { type: "password",  name: "password", placeholder: "Password", labelText: "Password", hasPreview: false, validator: AuthValidator.password }, 
+        { type: "password",  name: "confirmPassword", placeholder: "Confirm Password", labelText: "Confirm Password", hasPreview: false, validator: AuthValidator.confirmPassword }, 
+    ]
 
     const onSubmitHandler = (e) => {
         e.preventDefault();
@@ -23,31 +23,9 @@ const Register = () => {
     }
 
     return (
-        <AuthForm mainTitle="Learn & Create" title="Register" subtitle="Get prepared for your next job interview!" fields={fields} submitButtonText={"Register"} onSubmitParentHandler={onSubmitHandler}/>
+        <AuthForm mainTitle="Learn & Create" title="Register" subtitle="Get prepared for your next job interview!" 
+            fields={fields} submitButtonText={"Register"} onSubmitParentHandler={onSubmitHandler}/>
     )
 }
 
 export default Register;
-
-
-/*
-const onSubmit = (e) => {
-        e.preventDefault();
-
-        const formData = new FormData(e.target);
-
-        const email = formData.get('email');
-        const password = formData.get('password');
-        const confirmPassword = formData.get('confirm-password');
-
-        if (password !== confirmPassword) {
-            return;
-        }
-
-        authService.register(email, password)
-            .then(authData => {
-                auth.userLogin(authData);
-                navigate('/');
-            });
-    }
-*/
--- iquiz/src/helpers/validators/AuthValidator.js
+++ iquiz/src/helpers/validators/AuthValidator.js
@@ -2,7 +2,7 @@ class AuthValidator {
     static fullName(value) {
         console.log("value", value);
         if (!value || value.count === 0) {
-            return { valid: false, message: "Full Name is required!", value: value }
+            return { valid: false, message: "Full Name is required!" }
         }
 
         if (value.count < 3 || value.count > 25) {
@@ -10,6 +10,30 @@ class AuthValidator {
         }
 
         return { valid: true, message: "" }
+    }
+
+    static password(value) {
+        if (!value || value.count === 0) {
+            return { valid: false, message: "Password is required!" }
+        }
+
+        if (value.count < 8 || value.count > 20) {
+            return { valid: false, message: "Password should be between 8 and 20 characters long!"}
+        }
+
+        return { valid: true, message: "" }
+    }
+
+    static confirmPassword(value, comparableValue) {
+        if (!value || value.count === 0) {
+            return { valid: false, message: "Confirm Password is required!" }
+        }
+
+        if (value !== comparableValue) {
+            return { valid: false, message: "Both passwords doesn't match!" }
+        }
+
+        return { value: true, message: "" }
     }
 }
 
