8e91731c6a16b4589f1a1e7ff7e3c5a514a96efc
Emilia Nedyalkova <emilia_nedialkova@abv.bg>
2022-08-20 19:31:31 +0300

    add Question List Item as a separate component

--- iquiz/src/components/Quiz/Questions/QuestionList/QuestionList.js
+++ iquiz/src/components/Quiz/Questions/QuestionList/QuestionList.js
@@ -1,67 +1,26 @@
 import './QuestionList.css';
-import { Link } from 'react-router-dom';
+import QuestionListItem from './QuestionListItem/QuestionListItem';
 
 const QuestionList = () => {
     let questionList = [
-        { title: "Some really easy question", level: "easy", addedAt: "25/08/2021", answeredBy: 1001, successRate: "95.02" },
-        { title: "Some really hard question", level: "hard", addedAt: "02/04/2021", answeredBy: 1001, successRate: "95.02" },
-        { title: "Some medium question", level: "medium", addedAt: "93/01/2022", answeredBy: 1001, successRate: "95.02" },
-        { title: "Some really easy question", level: "easy", addedAt: "25/08/2021", answeredBy: 1001, successRate: "95.02" },
-        { title: "Some medium quetion", level: "medium", addedAt: "25/08/2021", answeredBy: 1001, successRate: "95.02" }
+        { title: "Some really easy question", level: "easy", addedAt: "25/08/2021", answeredBy: 1001, successRate: "95.02", id: 1 },
+        { title: "Some really hard question", level: "hard", addedAt: "02/04/2021", answeredBy: 1001, successRate: "95.02", id: 2 },
+        { title: "Some medium question", level: "medium", addedAt: "93/01/2022", answeredBy: 1001, successRate: "95.02", id: 3 },
+        { title: "Some really easy question", level: "easy", addedAt: "25/08/2021", answeredBy: 1001, successRate: "95.02", id: 4 },
+        { title: "Some medium quetion", level: "medium", addedAt: "25/08/2021", answeredBy: 1001, successRate: "95.02", id: 5 }
     ]
     return (
         <div className="question-list-container">
             <h1>Question List</h1>
             <div className="question-list-content">
-                <div className="question-list-item">
-                    <div className="question-info">
-                        <h2 className="question-title">The question content</h2>
-                        <div className="question-props">
-                            <div className="level">
-                                <span className="level-label">Level</span>
-                                <span className="level-diff easy"></span>
-                                <span className="level word">Easy</span>
-                            </div>
-                            <div className="date-added">
-                                <span className="icon">
-                                    <i class="fa-solid fa-calendar-plus"></i>
-                                </span>
-                                <span className="date">
-                                    25/08/2021
-                                </span>
-                            </div>
-                            <div className="answered-times">
-                                <span className="icon">
-                                    <i class="fa-solid fa-people-group"></i>
-                                </span>
-                                <span className="answered">
-                                    10144141
-                                </span>
-                            </div>
-                            <div className="success-rate">
-                                <span className="icon">
-                                    <i class="fa-solid fa-star"></i>
-                                </span>
-                                <span className="rate">
-                                    95.55%
-                                </span>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="actions">
-                        <button className="button" type="button">Solve</button>
-                        <Link className="button-action" to="/login">
-                            <span className="icon">
-                            <i class="fa-solid fa-pencil"></i>
-                            </span>
-                        </Link>
-                        <Link className="button-action" to="/register">
-                            <span className="icon">
-                            <i class="fa-solid fa-trash"></i>
-                            </span>
-                        </Link>
-                    </div>
-                </div>
+                {
+                    questionList.map(question => {
+                        console.log(question)
+                        return ( <QuestionListItem id={question.id} key={question.id} title={question.title} 
+                            level={question.level} addedAt={question.addedAt} answeredBy={question.answeredBy}  successRate={question.successRate}/>)
+                    })
+                }
+                
             </div>
         </div>
 
---
+++ iquiz/src/components/Quiz/Questions/QuestionList/QuestionListItem/QuestionListItem.js
@@ -0,0 +1,62 @@
+import './QuestionListItem.css';
+import { Link } from 'react-router-dom';
+
+const QuestionListItem = ({ id, title, level, addedAt, answeredBy, successRate}) => {
+    let levelWord = level.charAt(0).toUpperCase() + level.slice(1);
+
+    return (
+        <div className="question-list-item">
+            <div className="question-info">
+                <h2 className="question-title">{ title }</h2>
+                <div className="question-props">
+                    <div className="level">
+                        <span className="level-label">Level</span>
+                        <span className={`level-diff ${level}`}></span>
+                        <span className="level word">{ levelWord }</span>
+                    </div>
+                    <div className="date-added">
+                        <span className="icon">
+                            <i className="fa-solid fa-calendar-plus"></i>
+                        </span>
+                        <span className="date">
+                            { addedAt }
+                        </span>
+                    </div>
+                    <div className="answered-times">
+                        <span className="icon">
+                            <i className="fa-solid fa-people-group"></i>
+                        </span>
+                        <span className="answered">
+                            { answeredBy }
+                        </span>
+                    </div>
+                    <div className="success-rate">
+                        <span className="icon">
+                            <i className="fa-solid fa-star"></i>
+                        </span>
+                        <span className="rate">
+                            { `${successRate}%`}
+                        </span>
+                    </div>
+                </div>
+            </div>
+            <div className="actions">
+                <button className="button" type="button">Solve</button>
+                <div className="button-action">
+                    <Link className="button-action" to="/login">
+                        <span className="icon">
+                            <i className="fa-solid fa-pencil"></i>
+                        </span>
+                    </Link>
+                    <Link className="button-action" to="/register">
+                        <span className="icon">
+                            <i className="fa-solid fa-trash"></i>
+                        </span>
+                    </Link>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default QuestionListItem;
