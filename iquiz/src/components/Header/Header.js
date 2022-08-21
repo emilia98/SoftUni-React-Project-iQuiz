f8299c8bd37c1372b844ddea8bd353865c2705cb
Emilia Nedyalkova <emilia_nedialkova@abv.bg>
2022-08-21 04:14:50 +0300

    change Nav to show only proper links

--- iquiz/src/components/Header/Header.js
+++ iquiz/src/components/Header/Header.js
@@ -1,14 +1,23 @@
 import './Header.css';
 import { Link } from 'react-router-dom';
+import { useAuthContext } from '../../contexts/AuthContext';
 
 const Header = () => {
+    let { user } = useAuthContext();
     return (
         <nav>
             <div className="main-nav-menu">
-                <span className="menu-item"> <Link to="/">Home</Link> </span> 
-                <span className="menu-item"> <Link to="/login">Login</Link> </span> 
-                <span className="menu-item"> <Link to="/register">Register</Link> </span> 
-                <span className="menu-item"> <Link to="/create">Create</Link> </span> 
+                <div className="main-links">
+                    <span className="menu-item"> <Link to="/">Home</Link> </span>
+                    {user.username ? <span className="menu-item"> <Link to="/create">Create</Link> </span> : null}
+                </div>
+                {user.username ?
+                    null :
+                    <div className="auth-links">
+                        <span className="menu-item"> <Link to="/login">Login</Link> </span>
+                        <span className="menu-item"> <Link to="/register">Register</Link> </span>
+                    </div>
+                }
             </div>
         </nav>
     )
